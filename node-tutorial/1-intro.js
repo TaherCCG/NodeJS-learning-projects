2ffe2158e5ae4633589057ef51de4c5f3d57f748
TaherCCG <taher.ccg@gmail.com>
2024-12-15 19:20:13 +0000

    Add introductory Node.js script to demonstrate basic conditional logic

---
+++ node-tutorial/1-intro.js
@@ -0,0 +1,14 @@
+const amount = 12;
+
+if (amount < 10) {
+  console.log("small number");
+} else {
+  console.log("large number");
+}
+
+console.log(`hey it's my first node app!!!`);
+
+// Run the file in the terminal using the following command:
+// node 1-intro.js
+// (ensure you are in the correct directory in the terminal or provide the full path to the file)
+// example: node node-tutorial/1-intro.js
