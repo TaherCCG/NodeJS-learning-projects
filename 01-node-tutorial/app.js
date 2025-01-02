36941a973ad2fc5fd96dfa8f9e80f4837cef45d8
TaherCCG <taher.ccg@gmail.com>
2025-01-02 17:58:48 +0000

    Implement custom EventEmitter with response event handling

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,36 +1,16 @@
-const { readFile, writeFile } = require("fs").promises;
-// const util = require('util')
-// const readFilePromise = util.promisify(readFile)
-// const writeFilePromise = util.promisify(writeFile)
+const customEmitter = new EventEmitter();
 
-const start = async () => {
-  try {
-    const first = await readFile("./content/first.txt", "utf8");
-    const second = await readFile("./content/second.txt", "utf8");
-    await writeFile(
-      "./content/result-mind-grenade.txt",
-      `THIS IS AWESOME : ${first} ${second}`,
-      { flag: "a" }
-    );
-    console.log(first, second);
-  } catch (error) {
-    console.log(error);
-  }
-};
+// on and emit methods
+// keep track of the order
+// additional arguments
+// built-in modules utilize it
 
-start();
+customEmitter.on("response", (name, id) => {
+  console.log(`data recieved user ${name} with id:${id}`);
+});
 
-// const getText = (path) => {
-//   return new Promise((resolve, reject) => {
-//     readFile(path, "utf8", (err, data) => {
-//       if (err) {
-//         reject(err);
-//       } else {
-//         resolve(data);
-//       }
-//     });
-//   });
-// };
-// getText("./content/first.txt")
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err));
+customEmitter.on("response", () => {
+  console.log("some other logic here");
+});
+
+customEmitter.emit("response", "john", 34);
