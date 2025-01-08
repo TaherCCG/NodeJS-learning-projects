6da19b00e3f8b1403168b84629cb2be8772ed7dd
TaherCCG <taher.ccg@gmail.com>
2025-01-08 22:37:58 +0000

    Add Task model and implement createTask controller

--- 03-task-manager/app/controllers/tasks.js
+++ 03-task-manager/app/controllers/tasks.js
@@ -1,9 +1,12 @@
+const Task = require("../models/Task");
+
 const getAllTasks = (req, res) => {
   res.send("All Items from the file");
 };
 
-const createTask = (req, res) => {
-  res.send("Create Task");
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
 };
 
 const getTask = (req, res) => {
