e9b00034dd6ea617d01ff572debf90f4c658eda5
TaherCCG <taher.ccg@gmail.com>
2025-01-14 20:26:09 +0000

    Set up Express server with basic routes and middleware for error handling

--- 04-store-api/app/app.js
+++ 04-store-api/app/app.js
@@ -1,1 +1,35 @@
-console.log('04 Store API')
+require("dotenv").config();
+// async errors
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>products route</a>");
+});
+
+// Products routes
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
