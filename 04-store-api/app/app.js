343821ad330022bc054b3ee720866f8e1290d1b2
TaherCCG <taher.ccg@gmail.com>
2025-01-14 20:42:15 +0000

    Connect to MongoDB in the Express server and improve error logging

--- 04-store-api/app/app.js
+++ 04-store-api/app/app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const connectDB = require("./db/connect");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -24,11 +25,12 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
-      console.log(`Server is listening on port ${port}...`);
+      console.log(`Server is running on port ${port}...`);
     });
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
 
