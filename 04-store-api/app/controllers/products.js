604b9f3da8d5372efa5701074122bdd3bd1cdadf
TaherCCG <taher.ccg@gmail.com>
2025-01-15 23:41:31 +0000

    Add field selection support to product retrieval

--- 04-store-api/app/controllers/products.js
+++ 04-store-api/app/controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -23,11 +23,18 @@ const getAllProducts = async (req, res) => {
 
   let result = Product.find(queryObject);
 
+  // Sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
     result = result.sort(sortList);
   } else {
     result = result.sort("createdAt");
+  }
+
+  // Select fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
   }
 
   const products = await result;
