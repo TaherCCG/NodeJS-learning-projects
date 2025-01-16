46cf805d48ffd57352bcedc62ae13f1e7965f493
TaherCCG <taher.ccg@gmail.com>
2025-01-16 18:32:03 +0000

    Add support for filtering products by price and rating

--- 04-store-api/app/controllers/products.js
+++ 04-store-api/app/controllers/products.js
@@ -33,8 +33,15 @@ const getAllProducts = async (req, res) => {
       regEx,
       (match) => `-${operatorMap[match]}-`
     );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
   }
-
+  console.log(queryObject);
   let result = Product.find(queryObject);
 
   // Sort
