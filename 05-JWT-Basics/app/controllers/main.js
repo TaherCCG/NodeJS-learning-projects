85df263a785becbcc1a15a1d757586c8b957c5d0
TaherCCG <taher.ccg@gmail.com>
2025-01-20 12:27:32 +0000

    Refactor: replace CustomAPIError with specific error classes for better error handling

--- 05-JWT-Basics/app/controllers/main.js
+++ 05-JWT-Basics/app/controllers/main.js
@@ -1,10 +1,10 @@
 const jwt = require("jsonwebtoken");
-const CustomAPIError = require("../errors/custom-error");
+const { BadRequestError } = require("../errors");
 
 const login = async (req, res) => {
   const { username, password } = req.body;
   if (!username || !password) {
-    throw new CustomAPIError("Please provide username and password", 400);
+    throw new BadRequestError("Please provide username and password");
   }
 
   // for demo purposes
@@ -16,13 +16,10 @@ const login = async (req, res) => {
   const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
-  console.log(username, password);
   res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
-  console.log(req.user);
-
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, ${req.user.username}!`,
