e9a05fbd5895e170116027b1c28f9fab6f12af1f
TaherCCG <taher.ccg@gmail.com>
2025-02-28 22:20:24 +0000

    Add custom error handling for validation errors in middleware

--- 06-jobs-api/app/middleware/error-handler.js
+++ 06-jobs-api/app/middleware/error-handler.js
@@ -9,6 +9,12 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   // if (err instanceof CustomAPIError) {
   //   return res.status(err.statusCode).json({ msg: err.message })
   // } for reference - can remove
+  if (err.name && err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+    customError.statusCode = 400;
+  }
 
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
@@ -16,7 +22,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     )} field, please choose another value`;
     customError.statusCode = 400;
   }
-
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
