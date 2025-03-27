d623b2ec701c6942dde6e5c40dd314b03d0f3b3b
TaherCCG <taher.ccg@gmail.com>
2025-03-27 19:37:12 +0000

    Add test user identification in authentication middleware

--- 06.5-jobster-api/app/middleware/authentication.js
+++ 06.5-jobster-api/app/middleware/authentication.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 const { UnauthenticatedError } = require("../errors");
+require("dotenv").config();
 
 const auth = async (req, res, next) => {
   // check header
@@ -13,7 +14,8 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
     // attach the user to the job routes
-    req.user = { userId: payload.userId, name: payload.name };
+    const testUser = payload.userId === process.env.TEST_USER_ID;
+    req.user = { userId: payload.userId, testUser };
     next();
   } catch (error) {
     throw new UnauthenticatedError("Authentication invalid");
